f5fdefa97172c63be5b12774822ba542acd7ec7b
ryanliu6 <dio.ryanliu@gmail.com>
2018-10-04 16:59:07 -0400

    Have all but timeout

--- src/asst1/client.py
+++ src/asst1/client.py
@@ -1,14 +1,17 @@
+import time
 import socket
 
 # Defining UDP server variables
 UDP_PORT = 12000
-UDP_PACKET_SIZE = 2048
-UDP_SERVER = "localhost"
+UDP_PACKET_SIZE = 4096
+UDP_SERVER = "127.0.0.1"
 UDP_ADDRESS = (UDP_SERVER, UDP_PORT)
 
 # Message information
 NUM_MESSAGES = 10
 BASE_MESSAGE = "PING"
+SENT_TIME = 0
+RECV_TIME = 0
 
 def client():
     # Creates the socket
@@ -17,12 +20,17 @@ def client():
     print("Client Started")
 
     for i in range(NUM_MESSAGES):
-        currMsg = input(BASE_MESSAGE + " " + str(i))
+        currMsg = str.encode(BASE_MESSAGE + " " + str(i))
 
         clientSocket.sendto(currMsg, UDP_ADDRESS)
-        retMessage, serverAddress = clientSocket.recvfrom(UDP_PACKET_SIZE)
+        SENT_TIME = time.time()
 
-        print(retMessage.decode())
+        retMessage, serverAddress = clientSocket.recvfrom(UDP_PACKET_SIZE)
+        RECV_TIME = time.time()
+
+        print("Return Message: " + retMessage.decode())
+
+        print("This took %.2f ms" % (RECV_TIME*1000 - SENT_TIME*1000))
 
     clientSocket.close()
     print("Client Terminated")
--- src/asst1/server.py
+++ src/asst1/server.py
@@ -1,31 +1,55 @@
+import time
 import socket
+import random
 
 # Defining UDP server variables
 UDP_PORT = 12000
-UDP_PACKET_SIZE = 2048
-UDP_SERVER = "localhost"
+UDP_PACKET_SIZE = 4096
+UDP_SERVER = "127.0.0.1"
 UDP_ADDRESS = (UDP_SERVER, UDP_PORT)
 
 def server():
+    # Sets the random seed
+    random.seed(time.time())
+
     # Creates the socket
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Binds the socket to the defined port
-    serverAddress = (UDP_SERVER, UDP_PORT)
-    serverSocket.bind(serverAddress)
+    serverSocket.bind(UDP_ADDRESS)
 
-    print("Starting up on {} port {}".format(*serverAddress))
+    print("Starting up on {} port {}".format(*UDP_ADDRESS))
 
     while True:
         data, address = serverSocket.recvfrom(UDP_PACKET_SIZE)
 
-        print(data)
-        retData = data[::-1]
+        print('received {} bytes from {}'.format(
+            len(data), address))
+        print(data.decode())
 
-        if data:
-            serverSocket.sendto(retData, address)
+        randomWait()
+
+        if randomIgnore():
+            continue
         else:
-            print("Waiting for message")
+            serverSocket.sendto(data, address)
+
+def randomWait():
+    # Randomly wait for 5 to 50ms
+    delay = random.randint(5, 50)
+    print("Delay is: ", end = "")
+    print(delay)
+    time.sleep(delay / 1000)
+
+def randomIgnore():
+    # Ignores a message with a 10% chance
+    chance = random.randint(1, 10)
+
+    return False
+    # if chance == 1:
+    #     return True
+    # else:
+    #     return False
 
 if __name__ == "__main__":
     server()
